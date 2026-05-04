f683b8b0b535d71a4e0a06fdf4360ff2fa58fda9
Lukas Stavaris <lukas.stavaris@gmail.com>
2026-05-04 20:31:57 +0300

    Pythonic refactor

--- README.md
+++ README.md
@@ -27,11 +27,23 @@ The '**random**' library is imported and functions from it can be used, there ar
 An example strategy:
 ```python
 def example_strategy(my_moves, opponent_moves):
-    # Author: Joe
-    if opponent_moves:
+def example_strategy(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    Author: Joe
+
+    A strategy, that plays the opponent strategy last move, always starts
+    with rock
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    if 0 < len(opponent_moves):
         return opponent_moves[-1]
     else:
-        return 'rock'
+        return Move.ROCK
 ```
 
 To verify your strategies performace you can clone the repo, add your strategies function to the strategies.py file and run the run_tournament.py file.
--- strategies.py
+++ strategies.py
@@ -1,26 +1,92 @@
 import random
-import utils
 
-def random_strat(my_moves, opponent_moves):
-    choice = random.randint(0,2)
-    return utils.convert_to_name(choice)
+from utils import Move, MoveHistory
 
-def rock_or_paper(my_moves, opponent_moves):
-    choice = random.randint(0,1)
-    return utils.convert_to_name(choice)
+# pyright: reportUnusedParameter=false
 
-def rock_only(my_moves, opponent_moves):
-    return 'rock'
 
-def paper_only(my_moves, opponent_moves):
-    return 'paper'
+def random_strat(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    A primitive strategy which fully randomizes its moves, it is
+    interesting that this strategy is unexploitable, it will have
+    an equal score with all other strategies
 
-def scissors_only(my_moves, opponent_moves):
-    return 'scissors'
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    return random.choice(list(Move))
 
-def example_strategy(my_moves, opponent_moves):
-    # Author: Joe
-    if opponent_moves:
+
+def rock_or_paper(my_move: MoveHistory, opponent_move: MoveHistory) -> Move:
+    """
+    A primitive strategy which plays rock or scissors randomly,
+    a twist on random_strat, but should be way worse
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    return random.choice([Move.ROCK, Move.PAPER])
+
+
+def rock_only(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    A primitive and bad strategy, that only plays rock
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    return Move.ROCK
+
+
+def paper_only(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    A primitive and bad strategy, that only plays paper
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    return Move.PAPER
+
+
+def scissors_only(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    A primitive and bad strategy, that only plays scissors
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    return Move.SCISSORS
+
+
+def example_strategy(my_moves: MoveHistory, opponent_moves: MoveHistory) -> Move:
+    """
+    Author: Joe
+
+    A strategy, that plays the opponent strategy last move, always starts
+    with rock
+
+    Args:
+        my_moves: A history of this strategy moves this game
+        opponent_moves: A history of opponent strategy moves this game
+    Returns:
+        A move this strategy will make next
+    """
+    if 0 < len(opponent_moves):
         return opponent_moves[-1]
     else:
-        return 'rock'
+        return Move.ROCK
